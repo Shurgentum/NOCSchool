c0fa1f462560eb2bda54c7e14a4bcae4d7a2fe14
Shurgentum <k.asyeyev@gmail.com>
2021-03-15 21:07:15 +0200

    :)

--- backup.bash
+++ backup.bash
@@ -2,14 +2,24 @@
 VERSION="1.0.0"
 AUTHOR="Shurgentum"
 
-# Note: If you want to change the name of the destination folder, you should check it for existence
-DESTINATION_PATH="."
+# Note: Despite script creates destination folders, you should check it for existence manually
+DESTINATION_PATH="/.user_backups/"
+#
+LOG_PATH="~/log/"
+# Available date formats available in $(man date)
+DATE_FORMAT="%d.%m.%Y-%T"
 
 HELP_MESSAGE="This script is designed to create and save compressed backups of folders on remote using the SSH protocol. \n
 \n
-Usage: $(basename $0) [--option(-o) <argument>] <directory> \n
+Usage: \n
+\t $(basename $0) [--option(-o) <argument>] <directory> \n
+\n
+Examples: \n
+\t $(basename $0) -i \n
+\t $(basename $0) -a localhost <folder> -u root\n
 \n
 Options: \n
+\t [-i | --init ] - Init. \n
 \t [-h | --help ] - Show this screen. \n
 \t [-v | --version] - Show version. \n
 \t [-u | --user] - User [default: $USER]. \n
@@ -27,7 +37,7 @@ function parse() {
     key="$1"
     # Check if argument starts with "-" and save source folder if so
     if [[ $key == "-"* ]]; then
-      echo "" >> /dev/null
+      echo "" >>/dev/null
     else
       SOURCE="$1"
       break
@@ -43,12 +53,16 @@ function parse() {
       echo "$VERSION by $AUTHOR"
       exit 0
       ;;
+    -i | --init)
+      INIT=true
+      return
+      ;;
     -u | --user)
       USER="$2"
       shift 2
       ;;
     -a | --address)
-    # Expoort address for easier future use
+      # Expoort address for easier future use
       export BACKUP_ADDRESS="$2"
       shift 2
       ;;
@@ -61,26 +75,37 @@ function parse() {
   done
 }
 
-
-function copy() {
-  # if [[ -z "$BACKUP_ADDRESS" ]];
-  # then
-  #   scp -r $SOURCE $USER@$BACKUP_ADDRESS:~/$DESTINATION_PATH/$(basename $SOURCE)_$(date +"%d.%m.%Y-%T")
-  # else
-  #   echo "Destinaton address was not specified"
-  # fi
-
-  ARCHIEVE=./$(basename $1)_$(date +"%d.%m.%Y-%T").tar.gz
-  tar -czf $ARCHIEVE $1
-  scp $ARCHIEVE $USER@$BACKUP_ADDRESS:~/$DESTINATION_PATH
-  rm $ARCHIEVE
-  echo "$ARCHIEVE"
+function init() {
+  mkdir -p $LOG_PATH
+  ssh-copy-id $USER@$BACKUP_ADDRESS
+  ssh -oBatchMode=yes $USER@localhost "mkdir -p ~/$DESTINATION_PATH"
 }
 
-function log(){
-  mkdir ~/log
-  echo "Successfully "
+function logresult() {
+  if [[ $1 == 0 ]]; then
+    echo "[ $(date +"$DATE_FORMAT") ] -->> Successful backup of $ARCHIEVE to $USER@$BACKUP_ADDRESS:~/$DESTINATION_PATH" >>$LOG_PATH/homebackup.log
+  else
+    echo "[ $(date +"$DATE_FORMAT") ] -->> Backup failed with code $1 (scp) " >>$LOG_PATH/homebackup.log
+  fi
+}
+
+function copy() {
+  ARCHIEVE=./$(basename $1)_$(date +"$DATE_FORMAT").tar.gz
+  tar -czf $ARCHIEVE $1
+  # TODO: BACKUP_ADDRESS null check
+  scp $ARCHIEVE $USER@$BACKUP_ADDRESS:~/$DESTINATION_PATH
+
+}
+
+function checkspace() {
+  
 }
 
 parse $@
+if [[ $INIT ]]; then
+  init
+  return
+fi
+checkspace $SOURCE
 copy $SOURCE
+logresult $?
